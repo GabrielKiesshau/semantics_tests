b0afbebd2f9a308078c249a8ba77e212139f2e72
Gabriel H. Kiesshau <gabriel.kiesshau@gmail.com>
2023-08-01 16:02:59 -0300

    Tested with Inkwell

--- lib/hello.dart
+++ lib/hello.dart
@@ -8,41 +8,44 @@ class Hello extends StatelessWidget {
   Widget build(BuildContext context) {
     return SemanticsFix(
       semanticsLabel: 'página inteira',
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 22.5, vertical: 18),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(16.0),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text('2'),
-            const SizedBox(height: 13),
-            Semantics(
-              container: true,
-              child: ListTile(
-                leading: IconButton(
-                  onPressed: () => debugPrint('a'),
-                  icon: const Icon(
-                    Icons.calendar_month,
-                    semanticLabel: 'teste',
+      child: InkWell(
+        onTap: () => debugPrint('inkwell'),
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 22.5, vertical: 18),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(16.0),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text('2'),
+              const SizedBox(height: 13),
+              Semantics(
+                container: true,
+                child: ListTile(
+                  leading: IconButton(
+                    onPressed: () => debugPrint('a'),
+                    icon: const Icon(
+                      Icons.calendar_month,
+                      semanticLabel: 'teste',
+                    ),
                   ),
-                ),
-                title: Semantics(
-                  label: 'a',
-                  child: const Text(
-                    '3',
-                    semanticsLabel: '4',
+                  title: Semantics(
+                    label: 'a',
+                    child: const Text(
+                      '3',
+                      semanticsLabel: '4',
+                    ),
                   ),
-                ),
-                subtitle: const Text(
-                  '5',
-                  semanticsLabel: '6',
+                  subtitle: const Text(
+                    '5',
+                    semanticsLabel: '6',
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
